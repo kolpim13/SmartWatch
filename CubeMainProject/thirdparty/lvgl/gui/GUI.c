7a9cc683251f330416207f43de5a2bcf14f846d4
Maksym Torhunakov <torgunakovmaks2@gmail.com>
2024-12-08 16:42:09 +0100

    GUI - Settings Page added

--- CubeMainProject/thirdparty/lvgl/gui/GUI.c
+++ CubeMainProject/thirdparty/lvgl/gui/GUI.c
@@ -11,8 +11,6 @@ void GUI_Init(void)
 	lv_obj_t* tileview = lv_tileview_create(screen);
 	lv_obj_set_scrollbar_mode(tileview, LV_SCROLLBAR_MODE_OFF);
 
-	// lv_obj_set_size(tileview, lv_obj_get_width(lv_scr_act()), lv_obj_get_height(lv_scr_act()));
-
 	/* Add tiles (can be considered as pages / screens). */
 	lv_obj_t* tile_main = lv_tileview_add_tile(tileview, 0, 0, LV_DIR_RIGHT);
     lv_obj_t* tile_settings = lv_tileview_add_tile(tileview, 1, 0, LV_DIR_LEFT);
@@ -30,13 +28,5 @@ void GUI_Init(void)
 
 	/* Create status bar on top */
 	GUI_StatusBar_Create();
-}
-/*=================================================================*/
-
-/* LVGL TIMERS */
-void timer_statusBar_update(lv_timer_t* timer)
-{
-	(void)timer;
-	GUI_StatusBar_Update_Cyclic_1s();
 }
 /*=================================================================*/
--- CubeMainProject/thirdparty/lvgl/gui/GUI_SettingsPage.c
+++ CubeMainProject/thirdparty/lvgl/gui/GUI_SettingsPage.c
@@ -1,6 +1,129 @@
 #include "GUI_SettingsPage.h"
 
+#include  "../modules/ST7789/ST7789.h"
+
+typedef enum{
+    User_Data_Display_Brightness = 0,
+} User_Data_t;
+/*=================================================================*/
+
 static lv_obj_t* settings_page = NULL;
+static lv_obj_t* menu;
+static lv_obj_t* menu_main_page;
+static lv_obj_t* menu_display_page;
+
+static uint8_t light_prev;
+/*=================================================================*/
+
+/* MENU PAGES */
+static void menu_page_main_create(void);
+static void menu_page_display_create(void);
+
+/* AUXILIARY FUNCTIONS */
+// ...
+
+/* DISPLAY */
+static void event_display_slider_brightness_cb(lv_event_t* event);
+static void event_display_btn_apply_cb(lv_event_t* event);
+static void event_display_btn_cancel_cb(lv_event_t* event);
+/*=================================================================*/
+
+/* MENU PAGES */
+static void menu_page_main_create()
+{
+    lv_obj_t* content;
+    lv_obj_t* label;
+
+    menu_main_page = lv_menu_page_create(menu, NULL);
+    
+    content = lv_menu_cont_create(menu_main_page);
+    label = lv_label_create(content);
+    lv_label_set_text(label, "Display");
+    lv_menu_set_load_page_event(menu, content, menu_display_page);
+
+    content = lv_menu_cont_create(menu_main_page);
+    label = lv_label_create(content);
+    lv_label_set_text(label, "Time & Date [TBD]");
+
+    content = lv_menu_cont_create(menu_main_page);
+    label = lv_label_create(content);
+    lv_label_set_text(label, "System [TBD]");   /* Energy saving settings. */
+
+    content = lv_menu_cont_create(menu_main_page);
+    label = lv_label_create(content);
+    lv_label_set_text(label, "Info [TBD]");     /* Info about the project | SW & HW versions | CPU ID, etc. */
+}
+
+static void menu_page_display_create(void)
+{
+    lv_obj_t* content;
+    lv_obj_t* slider;
+    lv_obj_t* label;
+    lv_obj_t* btn;
+
+    /* Menu page */
+    menu_display_page = lv_menu_page_create(menu, NULL);
+    content = lv_menu_cont_create(menu_display_page);
+    lv_obj_set_flex_flow(content, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_flex_align(content, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+    /* Brightness */
+    light_prev = ST7789_GetLight();
+
+    lv_obj_t *cont_brightness = lv_obj_create(content);
+    lv_obj_set_size(cont_brightness, lv_pct(100), LV_SIZE_CONTENT);
+    lv_obj_set_flex_flow(cont_brightness, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_flex_align(cont_brightness, LV_FLEX_ALIGN_CENTER,  LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER);
+
+    label = lv_label_create(cont_brightness);
+    lv_label_set_text(label, "Brightness : ");
+    slider = lv_slider_create(cont_brightness);
+    lv_obj_set_width(slider, lv_pct(100));
+    lv_slider_set_value(slider, light_prev, LV_ANIM_OFF);
+    lv_obj_add_event_cb(slider, event_display_slider_brightness_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
+    /* New setting [If some appear] */
+    // ...
+
+    /* Footer */
+    btn = lv_msgbox_add_footer_button(content, "Apply");
+    lv_obj_set_flex_grow(btn, 1);
+    lv_obj_add_event_cb(btn, event_display_btn_apply_cb, LV_EVENT_CLICKED, (void *)slider);
+
+    btn = lv_msgbox_add_footer_button(content, "Cancel");
+    lv_obj_set_flex_grow(btn, 1);
+    lv_obj_add_event_cb(btn, event_display_btn_cancel_cb, LV_EVENT_CLICKED, (void *)slider);
+}
+
+/* AUXILIARY FUNCTIONS */
+// ...
+
+/* DISPLAY */
+static void event_display_slider_brightness_cb(lv_event_t* event)
+{
+    lv_obj_t *slider = lv_event_get_target(event);
+    uint8_t light = lv_bar_get_value(slider);
+    ST7789_SetLight(light);
+}
+
+static void event_display_btn_apply_cb(lv_event_t* event)
+{
+    lv_obj_t *slider = lv_event_get_user_data(event);
+    uint8_t light = lv_bar_get_value(slider);
+
+    /* Send data to be saved */
+    // ...
+
+    lv_menu_set_page(menu, menu_main_page);
+}
+
+static void event_display_btn_cancel_cb(lv_event_t* event)
+{
+    lv_obj_t *slider = lv_event_get_user_data(event);
+    ST7789_SetLight(light_prev);
+    lv_slider_set_value(slider, light_prev, LV_ANIM_OFF);
+    lv_menu_set_page(menu, menu_main_page);
+}
 /*=================================================================*/
 
 void GUI_SettingsPage_Create(lv_obj_t* parent)
@@ -10,17 +133,30 @@ void GUI_SettingsPage_Create(lv_obj_t* parent)
         return;
     }
 
+    lv_obj_t* content;
+    lv_obj_t* label;
+
     /* Page itself */
     settings_page = lv_obj_create(parent);
     lv_obj_set_size(settings_page, lv_obj_get_width(lv_scr_act()), lv_obj_get_height(lv_scr_act()));
     lv_obj_center(settings_page);
     lv_obj_set_scrollbar_mode(settings_page, LV_SCROLLBAR_MODE_OFF);
 
-    /*  */
-    lv_obj_t* time_label = lv_label_create(settings_page);
-    lv_obj_set_width(time_label, lv_display_get_horizontal_resolution(NULL) - 10);
-    lv_obj_set_style_text_align(time_label, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_set_style_text_font(time_label, &lv_font_montserrat_24, 0);
-    lv_obj_align(time_label, LV_ALIGN_TOP_MID, 0, 40);
-    lv_label_set_text(time_label, "Bla-bla-bla");
+    /* Menu */
+    menu = lv_menu_create(settings_page);
+    lv_menu_set_mode_header(menu, LV_MENU_HEADER_TOP_FIXED);
+    lv_menu_set_mode_root_back_button(menu, LV_MENU_ROOT_BACK_BUTTON_DISABLED);
+    lv_obj_set_size(lv_menu_get_main_header(menu), 0, 0);
+    lv_obj_set_size(lv_menu_get_main_header_back_button(menu), 0, 0);
+    lv_obj_set_size(menu, lv_obj_get_width(lv_scr_act()), lv_obj_get_height(lv_scr_act() - 30));
+    lv_obj_align(menu, LV_ALIGN_TOP_MID, 0, 30);
+    lv_obj_set_style_border_opa(menu, LV_OPA_TRANSP, 0);
+    lv_obj_set_scrollbar_mode(menu, LV_SCROLLBAR_MODE_OFF);
+
+    /* Menu pages */
+    menu_page_display_create();
+    menu_page_main_create();
+
+    /* Set main page as default */
+    lv_menu_set_page(menu, menu_main_page);
 }
