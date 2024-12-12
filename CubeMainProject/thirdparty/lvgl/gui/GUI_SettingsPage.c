499d0d13f189bc63684ac929e3c038d8a75660aa
Maksym Torhunakov <torgunakovmaks2@gmail.com>
2024-12-12 23:16:54 +0100

    GUI related update

--- CubeMainProject/thirdparty/lvgl/gui/GUI_SettingsPage.c
+++ CubeMainProject/thirdparty/lvgl/gui/GUI_SettingsPage.c
@@ -1,7 +1,8 @@
 #include "GUI_SettingsPage.h"
 
-#include  "../modules/nvm/nvm.h"
-#include  "../modules/ST7789/ST7789.h"
+#include "../modules/nvm/nvm.h"
+#include "../modules/RTC/RTC.h"
+#include "../modules/ST7789/ST7789.h"
 
 typedef enum{
     User_Data_Display_Brightness = 0,
@@ -15,26 +16,30 @@ static lv_obj_t* menu_display_page;
 static lv_obj_t* menu_datetime_page;
 
 static uint8_t light_prev;
+static RTC_TimeFormat_t time_format;
 /*=================================================================*/
 
-/* MENU PAGES */
+/* MAIN */
 static void menu_page_main_create(void);
-static void menu_page_display_create(void);
-static void menu_paage_datetime_create(void);
-
-/* AUXILIARY FUNCTIONS */
-// ...
 
 /* DISPLAY */
+static void menu_page_display_create(void);
 static void event_display_slider_brightness_cb(lv_event_t* event);
 static void event_display_btn_apply_cb(lv_event_t* event);
 static void event_display_btn_cancel_cb(lv_event_t* event);
 
 /* DATE & TIME */
+static void menu_page_datetime_create(void);
+static void event_datetime_switch_timeformat_cb(lv_event_t* event);
+static void event_datetime_btn_apply_cb(lv_event_t* event);
+static void event_datetime_btn_cancel_cb(lv_event_t* event);
+
+/* AUXILIARY FUNCTIONS */
+// ...
 
 /*=================================================================*/
 
-/* MENU PAGES */
+/* MAIN */
 static void menu_page_main_create()
 {
     lv_obj_t* content;
@@ -49,8 +54,8 @@ static void menu_page_main_create()
 
     content = lv_menu_cont_create(menu_main_page);
     label = lv_label_create(content);
-    lv_label_set_text(label, "Time & Date [TBD]");
-    //lv_menu_set_load_page_event(menu, content, menu_datetime_page);
+    lv_label_set_text(label, "Time & Date");
+    lv_menu_set_load_page_event(menu, content, menu_datetime_page);
 
     content = lv_menu_cont_create(menu_main_page);
     label = lv_label_create(content);
@@ -61,6 +66,7 @@ static void menu_page_main_create()
     lv_label_set_text(label, "Info [TBD]");     /* Info about the project | SW & HW versions | CPU ID, etc. */
 }
 
+/* DISPLAY */
 static void menu_page_display_create(void)
 {
     lv_obj_t* content;
@@ -84,6 +90,7 @@ static void menu_page_display_create(void)
 
     label = lv_label_create(cont_brightness);
     lv_label_set_text(label, "Brightness : ");
+
     slider = lv_slider_create(cont_brightness);
     lv_obj_set_width(slider, lv_pct(100));
     lv_slider_set_value(slider, light_prev, LV_ANIM_OFF);
@@ -102,23 +109,6 @@ static void menu_page_display_create(void)
     lv_obj_add_event_cb(btn, event_display_btn_cancel_cb, LV_EVENT_CLICKED, (void *)slider);
 }
 
-static void menu_paage_datetime_create(void)
-{
-    lv_obj_t* content;
-    lv_obj_t* label;
-    lv_obj_t* btn;
-
-    /* Menu page */
-    menu_datetime_page = lv_menu_page_create(menu, NULL);
-    content = lv_menu_cont_create(menu_datetime_page);
-    lv_obj_set_flex_flow(content, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_flex_align(content, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
-}
-
-/* AUXILIARY FUNCTIONS */
-// ...
-
-/* DISPLAY */
 static void event_display_slider_brightness_cb(lv_event_t* event)
 {
     lv_obj_t *slider = lv_event_get_target(event);
@@ -162,6 +152,106 @@ static void event_display_btn_cancel_cb(lv_event_t* event)
     lv_slider_set_value(slider, light_prev, LV_ANIM_OFF);
     lv_menu_set_page(menu, menu_main_page);
 }
+
+/* DATE & TIME */
+static void menu_page_datetime_create(void)
+{
+    lv_obj_t* content;
+    lv_obj_t* sw;
+    lv_obj_t* label;
+    lv_obj_t* btn;
+
+    /* Menu page */
+    menu_datetime_page = lv_menu_page_create(menu, NULL);
+    content = lv_menu_cont_create(menu_datetime_page);
+    lv_obj_set_flex_flow(content, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_flex_align(content, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+    /* Time format */
+    time_format = RTC_GetTimeFormat();
+
+    lv_obj_t *cont_timeformat = lv_obj_create(content);
+    lv_obj_set_size(cont_timeformat, lv_pct(100), LV_SIZE_CONTENT);
+    lv_obj_set_flex_flow(cont_timeformat, LV_FLEX_FLOW_ROW);
+    lv_obj_set_flex_align(cont_timeformat, LV_FLEX_ALIGN_START,  LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+    lv_obj_remove_flag(cont_timeformat, LV_OBJ_FLAG_SCROLLABLE);
+
+    label = lv_label_create(cont_timeformat);
+    lv_label_set_text(label, "Time foramt:");
+
+    sw = lv_switch_create(cont_timeformat);
+    label = lv_label_create(cont_timeformat);
+    if (time_format == RTC_TimeFormat_24H)
+    {
+        lv_label_set_text_static(label, "24H ");
+        lv_obj_remove_state(sw, LV_STATE_CHECKED);
+    }
+    else
+    {
+        lv_label_set_text_static(label, "12H ");
+        lv_obj_add_state(sw, LV_STATE_CHECKED);
+    }
+    lv_obj_add_event_cb(sw, event_datetime_switch_timeformat_cb, LV_EVENT_VALUE_CHANGED, (void *)label);
+
+    /* Footer */
+    btn = lv_msgbox_add_footer_button(content, "Apply");
+    lv_obj_set_flex_grow(btn, 1);
+    lv_obj_add_event_cb(btn, event_datetime_btn_apply_cb, LV_EVENT_CLICKED, NULL);
+
+    btn = lv_msgbox_add_footer_button(content, "Cancel");
+    lv_obj_set_flex_grow(btn, 1);
+    lv_obj_add_event_cb(btn, event_datetime_btn_cancel_cb, LV_EVENT_CLICKED, NULL);
+}
+
+static void event_datetime_switch_timeformat_cb(lv_event_t* event)
+{
+    lv_obj_t* sw = lv_event_get_target(event);
+    lv_obj_t* label = lv_event_get_user_data(event);
+
+    if (lv_obj_has_state(sw, LV_STATE_CHECKED) == true)
+    {
+        lv_label_set_text_static(label, "12H ");
+        time_format = RTC_HOURFORMAT_12;
+    }
+    else
+    {
+        lv_label_set_text_static(label, "24H ");
+        time_format = RTC_HOURFORMAT_24;
+    }
+}
+
+static void event_datetime_btn_apply_cb(lv_event_t* event)
+{
+    /* Change data format now */
+    RTC_SetTimeFormat(time_format);
+
+    /* Update status bar with new time format. */
+    GUI_StatusBar_Update();
+
+    /* Write to the NvM. */
+    NvM_Block_t nvm_block = NvM_Block_RTC;
+    nvm_ram.data.rtc.time_format = time_format;
+    if (xQueueSend(
+            EepromQueueHandle,
+            (void*)&nvm_block,
+            (TickType_t)0) != pdPASS)
+    {
+        /* Add some notification for the user */
+        // ...
+        ;
+    }
+
+    lv_menu_set_page(menu, menu_main_page);
+}
+
+static void event_datetime_btn_cancel_cb(lv_event_t* event)
+{
+    lv_menu_set_page(menu, menu_main_page);
+}
+
+/* AUXILIARY FUNCTIONS */
+// ...
+
 /*=================================================================*/
 
 void GUI_SettingsPage_Create(lv_obj_t* parent)
@@ -190,7 +280,7 @@ void GUI_SettingsPage_Create(lv_obj_t* parent)
 
     /* Menu pages */
     menu_page_display_create();
-    menu_paage_datetime_create();
+    menu_page_datetime_create();
     menu_page_main_create();
 
     /* Set main page as default */
